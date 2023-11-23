87f4d37a5ed473021a3aa4cc43bd48258323c4c7
ROBSON FERREIRA HERCULANO JUNIOR <robson_herculano@hotmail.com>
2023-11-22 21:05:10 -0300

    Ediçãode item

--- src/pages/admin/pages/UpdateProduct.jsx
+++ src/pages/admin/pages/UpdateProduct.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import myContext from '../../../context/data/myContext'
 
 function UpdateProduct() {
+    const context = useContext(myContext);
+    const {products,setProducts, updateProduct} = context;
     return (
         <div>
             <div className=' flex justify-center items-center h-screen'>
@@ -10,6 +13,8 @@ function UpdateProduct() {
                     </div>
                     <div>
                         <input type="text"
+                        value={products.title}
+                       onChange={(e) => setProducts({ ...products, title: e.target.value })}
                             name='title'
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product title'
@@ -17,27 +22,34 @@ function UpdateProduct() {
                     </div>
                     <div>
                         <input type="text"
+                        value={products.price}
                             name='price'
+                            onChange={(e) => setProducts({ ...products, price: e.target.value })} 
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product price'
                         />
                     </div>
                     <div>
                         <input type="text"
+                        value={products.imageUrl}
                             name='imageurl'
+                            onChange={(e) => setProducts({ ...products, imageUrl: e.target.value })} 
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product imageUrl'
                         />
                     </div>
                     <div>
                         <input type="text"
+                        value={products.category}
                             name='category'
+                            onChange={(e) => setProducts({ ...products, category: e.target.value })} 
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product category'
                         />
                     </div>
                     <div>
-                       <textarea cols="30" rows="10" name='title'
+                       <textarea cols="30" rows="10" 
+                       name='description' onChange={(e) => setProducts({ ...products, description: e.target.value })}
                             className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                             placeholder='Product title'>
 
@@ -45,6 +57,7 @@ function UpdateProduct() {
                     </div>
                     <div className=' flex justify-center mb-3'>
                         <button
+                        onClick={updateProduct}
                             className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
                             Update Product
                         </button>
